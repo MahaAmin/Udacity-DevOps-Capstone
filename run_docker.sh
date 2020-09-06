e4ce2fd516b75ac6c860e406dcb3ad6107ca6bbe
MahaAmin <mahaamin97@gmail.com>
2020-09-06 02:09:57 +0200

    add docker stage jenkinsfile

--- run_docker.sh
+++ run_docker.sh
@@ -1,5 +1,5 @@
 # Build flask-app image
-docker build -t flask-app .
+sudo docker build -t flask-app .
 
 # Run flask-app container
-docker run -p 5000:5000 --name flask-app flask-app
+sudo docker run -p 5000:5000 --name flask-app flask-app
