bba5bd263f5eff1d2a5fa5e2f919738ad3034474
MahaAmin <mahaamin97@gmail.com>
2020-09-06 02:13:58 +0200

    add jenkins user to docker group to run docker without sudo

--- run_docker.sh
+++ run_docker.sh
@@ -1,5 +1,5 @@
 # Build flask-app image
-sudo docker build -t flask-app .
+docker build -t flask-app .
 
 # Run flask-app container
-sudo docker run -p 5000:5000 --name flask-app flask-app
+docker run -p 5000:5000 --name flask-app flask-app
